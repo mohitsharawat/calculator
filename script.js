a7acb396b14c7ab698af8541e9369c3b8cb2b854
mohitsharawat <mohitsharawat500@gmail.com>
2021-06-18 10:35:11 +0530

    add  dark mod

--- script.js
+++ script.js
@@ -17,3 +17,22 @@ function clearscreen() {
       value;
     display.value = display.value + result;
   }
+
+
+  // dark mode
+
+  function dark_light() {
+    var element = document.getElementById('dark-check');
+    var dark = document.getElementById('container');
+
+    if(element.checked){
+        dark.classList.remove("light");
+        dark.classList.add("dark");
+    } else if(element.checked != true){
+        dark.classList.remove("dark");
+        dark.classList.add("light");
+    }
+    else{
+        alert("function not working");
+    }
+}
